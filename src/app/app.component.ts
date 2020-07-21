e1fcf3daefc035e518bb39fad801a917b4325e56
VaibhaviAgrawal <avaibhavi12345@gmail.com>
2020-07-22 00:09:44 +0530

    21-7 work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, FormArray , Validators, FormBuilder} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'form';
+export class AppComponent implements OnInit {
+
+  loginForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+
+  }
+
+  ngOnInit() {
+
+      this.loginForm = this.fb.group({
+
+          email: [null, [Validators.required, Validators.minLength(4)]],
+
+          password: [null, [Validators.required, Validators.maxLength(8)]]
+
+      })
+
+  }
+
+  loginUser() {
+
+      console.log(this.loginForm.status);
+
+      console.log(this.loginForm.value);
+
+  }
+
 }
